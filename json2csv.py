3b83708e788e451990949c682c3f9ec9da6729ae
sayandeep <sayan@sayan-ubuntu-18.04>
2018-10-02 19:30:55 +0530

    introduced pandas dataframe and export to excel format

--- json2csv.py
+++ json2csv.py
@@ -1,64 +1,69 @@
 import json
+import pandas as pd
+import itertools
 
-
-inputjson = open('newcustomer.json','r')
+inputjson = open('testTransaction.json','r')
 filterfile = open('customerfilter.csv','r')
 outfile = open('newcustomer.csv','w')
 
 data = json.load(inputjson)
 filterdata = []
 jsondata = data[0]
+datafrm = pd.DataFrame(columns=['fields','values'])
+global_counter1 = itertools.count()
 
-for line in filterfile:
-    filterdata.append((line.replace('"','')).replace('\n',''))
+#for line in filterfile:
+#    filterdata.append((line.replace('"','')).replace('\n',''))
 
-def filters(filters, keystr, value):
-    for data in range(len(filters)):
-        if filters[data] == keystr:
-            process_str(keystr, value)
+#def filters(filters, keystr, value):
+#    for data in range(len(filters)):
+#        if filters[data] == keystr:
+#            process_str(keystr, value)
 
-def process_str(keystring, value):
+def process_str(keystring, value, datafrm):
+    next_id = next(global_counter1)
+    datafrm.loc[next_id] = [keystring,value]
     outfile.write('"{}","{}"\n'.format(keystring, value))
 
 
-def process_list(keystring, value):
+def process_list(keystring, value, datafrm):
     counter = 0
     for item in range(len(value)):
-        if type(value[item]) is str:
+        if type(value[item]) not in [list, dict]:
             counter += 1
             key_counter = keystring+"_"+str(counter)
-            process_str(key_counter, value[item])
-        if type(value[item]) is int:
-            counter += 1
-            key_counter = keystring+"_"+str(counter)
-            process_str(key_counter, str(value[item]))
+            process_str(key_counter, value[item], datafrm)
         if type(value[item]) is dict:
-            process_dict(keystring, value[item])
+            process_dict(keystring, value[item], datafrm)
+        if type(value[item]) is list:
+            process_list(combined_key, value[item], datafrm)
 
 
-def process_dict(key, value):
+def process_dict(key, value, datafrm):
     for key1, val1 in value.items():
         combined_key = key+"."+key1 
-        if type(val1) is str:
-            process_str(combined_key, val1)
-        if type(val1) is int:
-            process_str(combined_key, val1)        
+        if type(val1)  not in [list, dict]:
+            process_str(combined_key, val1, datafrm)      
         if type(val1) is list:
-            process_list(combined_key, val1)
+            process_list(combined_key, val1, datafrm)
         if type(val1) is dict:
-            process_dict(combined_key, val1)
+            process_dict(combined_key, val1, datafrm)
+
 
 
 for key,val in jsondata.items():
-    if type(val) is str:
-        process_str(key, val)
+    if type(val)  not in [list, dict]:
+        process_str(key, val, datafrm)
     if type(val) is list:
         #print(key, val)
-        process_list(key, val)
+        process_list(key, val, datafrm)
     if type(val) is dict:
-        process_dict(key, val)         
+        process_dict(key, val, datafrm)        
     
 
 inputjson.close()
 filterfile.close()
 outfile.close()
+writer = pd.ExcelWriter('PythonExport.xlsx')
+datafrm.to_excel(writer,'Sheet1')
+writer.save()
